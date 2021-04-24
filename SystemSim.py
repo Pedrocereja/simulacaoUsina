48639c9bef4560c458b3f0202f6c83127d04f384
Pedrocereja <pedro_cardoso_c@hotmail.com>
2021-04-24 16:17:54 -0300

    Co-authored-by: Thomas Rodrigues de Souza <thomas.rs@ufsc.br>

--- SystemSim.py
+++ SystemSim.py
@@ -9,10 +9,22 @@ tau = 4.29
 a = tf(1, [Tn, 0])
 b = tf(1, [tau, 1])
 c = tf(1, [Th, 1])
-
 Nge = a*b*c
+print(Nge)
+
 
 t = np.linspace(0, 100, 1000)
 respostaImpulso, tempo = step(Nge, t)
 plt.plot(tempo, respostaImpulso)
+plt.show()
+
+bode(Nge, Hz = True)
+plt.show()
+
+rlocus(Nge)
+plt.show()
+
+realimentado = Nge/(1+Nge)
+r, t = step(realimentado)
+plt.plot(t, r)
 plt.show()
