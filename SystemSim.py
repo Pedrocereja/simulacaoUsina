d3cef45355ac11f51ffbc612d7f663a991156bc5
Pedrocereja <pedro_cardoso_c@hotmail.com>
2021-04-26 17:38:07 -0300

    Added git ignore and comments

--- SystemSim.py
+++ SystemSim.py
@@ -2,16 +2,17 @@ from control.matlab import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Variáveis a 100%
 Tn = 5.71
 Th = 0.71
 tau = 4.29
 
+# Função tranferência
 a = tf(1, [Tn, 0])
 b = tf(1, [tau, 1])
 c = tf(1, [Th, 1])
 Nge = a*b*c
 print(Nge)
-
 
 t = np.linspace(0, 100, 1000)
 respostaImpulso, tempo = step(Nge, t)
@@ -24,7 +25,7 @@ plt.show()
 rlocus(Nge)
 plt.show()
 
-realimentado = Nge/(1+Nge)
-r, t = step(realimentado)
+Ngerealimentado = Nge/(1+Nge)
+r, t = step(Ngerealimentado)
 plt.plot(t, r)
 plt.show()
